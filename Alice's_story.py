04abd3405ff3b85259639346881be5325e011130
sage25 <1300592689@qq.com>
2024-09-12 13:06:46 +0800

    test_9_12
    Alice is a whale, and her voice frequency is only 51, so other whales can't hear her. It has no friends.

---
+++ Alice's_story.py
@@ -0,0 +1,10 @@
+name = 'Alice'
+voice = 52
+age = 51
+wish = 'I can some friends'
+
+print('*'*10 + 'What Alice say'+ '*'*10)
+print("My name is "+name)
+print(f"I have a{voice} Hz voice")
+print(f"I am {age} year oide") 
+print("I wish " + wish)
