c424aa6d556ae8f0bcd40839bf3624e369ba0ac5
sage25 <1300592689@qq.com>
2024-09-23 21:11:33 +0800

    test_9_23

---
+++ test_9_23.py
@@ -0,0 +1,15 @@
+import random
+import math
+
+r = random.random()
+print("r = " , r)
+
+r = random.randint(1, 3)
+print("r = " , r)
+ 
+x = 0.1 + 0.2 +0.3
+print('x equals', x)
+
+x = math.fsum([0.1 ,0.2 ,0.3])
+print('x equals', x)
+
