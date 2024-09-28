d22f1ecf4ba2f7528085ef0e1122fbf7ddc8a137
sage25 <1300592689@qq.com>
2024-09-28 10:43:06 +0800

    Add files via upload

---
+++ test_9_28.py
@@ -0,0 +1,20 @@
+
+a = 5
+b = "hello"
+c = 3.2
+d = 0
+e = ""
+
+print("bool(a);",bool(a))
+print("bool(b);",bool(b))
+print("bool(c);",bool(c))
+print("bool(d);",bool(d))
+print("bool(e);",bool(e))
+
+age = int(input("Please input your age:"))
+if age >= 18:
+    print("derage")
+    if age == 18:    
+        print("congratulations")
+else:
+    print("underage")
