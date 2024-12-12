23f9393113afd8f75a82d7f572cad75c0b009981
sage25 <1300592689@qq.com>
2024-12-12 15:48:07 +0800

    Add files via upload

---
+++ test_12_12.py
@@ -0,0 +1,8 @@
+import jieba
+# txt = "中国是一个伟大的国家"
+# print(jieba.lcut(txt))
+
+
+# a = "重庆欢迎您!"
+# for i in range(0,int(len(a)/2)):
+#     print("{:口^20}".format(a[int(len(a)/2)-i-1:int(len(a)/2)+i+1]))
